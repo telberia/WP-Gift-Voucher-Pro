635d3b11102c21cfca8b589eeab9564716e96bea
quominh3707 <68995910+quominh3707@users.noreply.github.com>
2022-02-12 09:58:12 +0700

    Fix Production Environment

--- include/PayPalAuth.php
+++ include/PayPalAuth.php
@@ -2,8 +2,7 @@
 
 use PayPalCheckoutSdk\Core\PayPalHttpClient;
 use PayPalCheckoutSdk\Core\SandboxEnvironment;
-
-
+use PayPalCheckoutSdk\Core\ProductionEnvironment;
 
 class PayPalAuth
 {
@@ -13,12 +12,24 @@ class PayPalAuth
     }
     public static function environment()
     {
+        global $wpdb;
+        $setting_table  = $wpdb->prefix . 'giftvouchers_setting';
+        $setting_options = $wpdb->get_row( "SELECT * FROM $setting_table WHERE id = 1" );
+
+        $mode = (!$setting_options->test_mode) ? 'live' : 'sandbox';
+
         $wpgv_paypal_client_id = get_option('wpgv_paypal_client_id') ? get_option('wpgv_paypal_client_id') : '';
         $wpgv_paypal_secret_key = get_option('wpgv_paypal_secret_key') ? get_option('wpgv_paypal_secret_key') : ''; 
 
-        $clientId = getenv("CLIENT_ID") ?: $wpgv_paypal_client_id;
-        $clientSecret = getenv("CLIENT_SECRET") ?: $wpgv_paypal_secret_key;
+        if($mode == "sandbox"){
+            $clientId = getenv("CLIENT_ID") ?: $wpgv_paypal_client_id;
+            $clientSecret = getenv("CLIENT_SECRET") ?: $wpgv_paypal_secret_key;
+            return new SandboxEnvironment($clientId, $clientSecret);
+        }else if($mode == "live"){
+            $clientId = getenv("CLIENT_ID") ?: $wpgv_paypal_client_id;
+            $clientSecret = getenv("CLIENT_SECRET") ?: $wpgv_paypal_secret_key;
+            return new ProductionEnvironment($clientId, $clientSecret);
+        }
         
-        return new SandboxEnvironment($clientId, $clientSecret);
     }
 }
